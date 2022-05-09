caddb91b2a6eba9ff0da840be1426754690eb5ec
zcao001 <104553984+zcao001@users.noreply.github.com>
2022-05-09 10:32:27 +0200

    Update streamlit_app.py
    added something for food from Snowflake courses!

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('New learning now for me')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
